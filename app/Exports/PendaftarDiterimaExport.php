be25a0218832ea731f8ba8c63f778eb07b0ba0b9
ferdym02 <51710824+ferdym02@users.noreply.github.com>
2025-03-30 21:08:07 +0700

    Update PendaftarDiterimaExport.php

--- app/Exports/PendaftarDiterimaExport.php
+++ app/Exports/PendaftarDiterimaExport.php
@@ -9,16 +9,23 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class PendaftarDiterimaExport implements FromCollection, WithHeadings
 {
+    protected $periodeId;
+
+    public function __construct($periodeId)
+    {
+        $this->periodeId = $periodeId;
+    }
+
     public function collection()
     {
         $pendaftar = Pendaftar::whereIn('status_pendaftaran', ['diterima', 'cadangan'])
-            ->with('jurusanDiterima') // Load data jurusan
+            ->where('periode_id', $this->periodeId) // Filter berdasarkan periode yang dipilih
+            ->with('jurusanDiterima')
             ->orderBy('jurusan_diterima')
             ->orderBy('nilai_akhir', 'desc')
             ->get();
 
-        // Map data untuk menambahkan nomor urut dan nama jurusan
-        $dataWithNumbers = $pendaftar->map(function ($item, $index) {
+        return $pendaftar->map(function ($item, $index) {
             return [
                 'No' => $index + 1,
                 'Nomor Pendaftaran' => $item->nomor_pendaftaran,
@@ -26,18 +33,16 @@ class PendaftarDiterimaExport implements FromCollection, WithHeadings
                 'Nama Lengkap' => $item->nama_lengkap,
                 'Jenis Kelamin' => $item->jenis_kelamin,
                 'Asal Sekolah' => $item->asal_sekolah,
-                'Jurusan Diterima' => $item->jurusanDiterima ? $item->jurusanDiterima->nama : '-', // Tampilkan nama jurusan
+                'Jurusan Diterima' => $item->jurusanDiterima ? $item->jurusanDiterima->nama : '-',
                 'Nilai Akhir' => $item->nilai_akhir,
             ];
         });
-
-        return collect($dataWithNumbers);
     }
 
     public function headings(): array
     {
         return [
-            'No',                     // Menambahkan heading untuk nomor
+            'No',
             'Nomor Pendaftaran',
             'NISN',
             'Nama Lengkap',
