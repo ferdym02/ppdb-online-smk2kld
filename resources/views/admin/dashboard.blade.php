5a26cb31aeaed89e7cfb424c2210e4023ae09315
ferdym02 <51710824+ferdym02@users.noreply.github.com>
2025-04-15 01:12:35 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,7 +36,7 @@
                         </div>
                         <i class="fa fa-users small-box-icon"></i>
                         <a href="{{ route('admin.pendaftar.index') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            More info <i class="bi bi-link-45deg"></i>
+                            Info lengkap <i class="bi bi-link-45deg"></i>
                         </a>
                     </div>
                     <!--end::Small Box Widget 1-->
@@ -49,11 +49,11 @@
                     <div class="small-box text-bg-success">
                         <div class="inner">
                             <h3>{{ $totalDiterima }}</h3>
-                            <p>Pendaftar Diterima</p>
+                            <p>Pendaftar Lulus</p>
                         </div>
                         <i class="fa fa-check small-box-icon"></i>
-                        <a href="{{ route('pendaftar.status', 'diterima') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            More info <i class="bi bi-link-45deg"></i>
+                        <a href="{{ route('admin.pendaftar.index') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+                            Info lengkap <i class="bi bi-link-45deg"></i>
                         </a>
                     </div>
                     <!--end::Small Box Widget 2-->
@@ -66,11 +66,11 @@
                     <div class="small-box text-bg-danger">
                         <div class="inner">
                             <h3>{{ $totalGugur }}</h3>
-                            <p>Pendaftar Gugur</p>
+                            <p>Pendaftar Tidak Lulus</p>
                         </div>
                         <i class="fa fa-times small-box-icon"></i>
-                        <a href="{{ route('pendaftar.status', 'gugur') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            More info <i class="bi bi-link-45deg"></i>
+                        <a href="{{ route('admin.pendaftar.index') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+                            Info lengkap <i class="bi bi-link-45deg"></i>
                         </a>
                     </div>
                     <!--end::Small Box Widget 3-->
@@ -87,7 +87,7 @@
                         </div>
                         <i class="fas fa-list small-box-icon"></i>
                         <a href="{{ route('jurusan.index') }}" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            More info <i class="bi bi-link-45deg"></i>
+                            Info lengkap <i class="bi bi-link-45deg"></i>
                         </a>
                     </div>
                     <!--end::Small Box Widget 4-->
@@ -95,9 +95,34 @@
                 <!--end::Col-->
             </div>
             <!--end::Row-->
-            <!-- Informasi Sekolah -->
-            <div class="row justify-content-center my-3">
-                <div class="col-lg-6 text-center">
+            <div class="row">
+                <!-- Diagram Line (Lebih Lebar) -->
+                <div class="col-md-8">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <h3 class="card-title">Grafik Pendaftar</h3>
+                        </div>
+                        <div class="card-body d-flex justify-content-center align-items-center" style="min-height: 300px;">
+                            <canvas id="pendaftarChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            
+                <!-- Diagram Gender (Lebih Kecil) -->
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <h3 class="card-title">Pendaftar (L/P) Tahun {{ $tahunSekarang }}</h3>
+                        </div>
+                        <div class="card-body d-flex justify-content-center align-items-center" style="min-height: 300px;">
+                            <canvas id="genderChart"></canvas>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+            
+            <div class="card my-3">
+                <div class="card-body text-center">
                     <img src="{{ asset('storage/' . $schoolProfile->logo_sekolah) }}" alt="Logo Sekolah" class="img-fluid mb-1" style="max-width: 100px;">
                     <h4>{{ $schoolProfile->nama_sekolah }}</h4>
                     <p class="mb-0">{{ $schoolProfile->alamat_sekolah }}</p>
@@ -127,4 +152,75 @@
             toastError.show();
         @endif
     </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            let tahun = {!! json_encode(array_keys($pendaftarPerTahun)) !!}; // Tahun
+            let jumlahPendaftar = {!! json_encode(array_values($pendaftarPerTahun)) !!}; // Total pendaftar
+            let jumlahDiterima = {!! json_encode(array_values($diterimaPerTahun)) !!}; // Total diterima
+            let jumlahGugur = {!! json_encode(array_values($gugurPerTahun)) !!}; // Total gugur
+    
+            let ctx = document.getElementById('pendaftarChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: tahun, // Tahun sebagai label sumbu X
+                    datasets: [
+                        {
+                            label: 'Total Pendaftar',
+                            data: jumlahPendaftar, 
+                            borderColor: 'rgba(54, 162, 235, 1)',
+                            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                            borderWidth: 2,
+                            fill: true
+                        },
+                        {
+                            label: 'Lulus',
+                            data: jumlahDiterima, 
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                            borderWidth: 2,
+                            fill: false
+                        },
+                        {
+                            label: 'Tidak Lulus',
+                            data: jumlahGugur, 
+                            borderColor: 'rgba(255, 99, 132, 1)',
+                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                            borderWidth: 2,
+                            fill: false
+                        }
+                    ]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: { display: true }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            suggestedMax: 25 
+                        }
+                    }
+                }
+            });
+        });
+    </script>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var ctx = document.getElementById('genderChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ['Laki-laki', 'Perempuan'],
+                    datasets: [{
+                        data: [{{ $totalLakiLaki }}, {{ $totalPerempuan }}],
+                        backgroundColor: ['#36A2EB', '#FF6384']
+                    }]
+                }
+            });
+        });
+    </script>
+    
 @endsection
