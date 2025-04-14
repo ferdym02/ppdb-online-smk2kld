925adf99cc15e70b0fc92056bf8ea5c35c4c070e
ferdym02 <51710824+ferdym02@users.noreply.github.com>
2025-04-15 01:12:20 +0700

    Update 2025_01_06_212804_create_pengumumans_table.php

--- database/migrations/2025_01_06_212804_create_pengumumans_table.php
+++ database/migrations/2025_01_06_212804_create_pengumumans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pengumumans', function (Blueprint $table) {
             $table->id(); // Primary key (UNSIGNED BIGINT)
             $table->string('judul', 255); // Judul pengumuman
+            $table->longText('isi')->nullable();
             $table->string('file_lampiran', 255)->nullable(); // File lampiran pengumuman
             $table->timestamps(); // created_at and updated_at
         });
