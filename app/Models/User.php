284cd1d486cdc2b1f492a7f4861cfd9483124f6a
ferdym02 <51710824+ferdym02@users.noreply.github.com>
2025-05-30 09:36:20 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\VerifikasiEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -47,5 +48,10 @@ class User extends Authenticatable
     public function pendaftar()
     {
         return $this->hasOne(Pendaftar::class);
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifikasiEmail);
     }
 }
